8cae302da732d0e8dc8c4fb3e1f7fce394ebaa1f
Michal Nowak <michalkacpernowak@gmail.com>
2019-12-11 15:02:31 +0100

    A lot of shit

--- hangMan.py
+++ hangMan.py
@@ -1,5 +1,6 @@
 import random
 import time
+from os import system, name
 
 hangman = [r'''
              ____
@@ -50,9 +51,10 @@ lotery_word=[]
 locked_word=[]
 word=[]
 duplicated_char=[]
-index_to_unlock=1
 countries_capitals = []
 
+def clear():
+    system('clear')
 
 def splitting_countries():
     for line in open('countries-and-capitals.txt'):
@@ -103,6 +105,7 @@ def checking_char(x, y):
         return duplicated_char
 
 def unlocking_char(x, list, lock, duplicated_char):
+    index_to_unlock=1
     lenght=len(duplicated_char)
     while lenght !=0:
         index_to_unlock=duplicated_char[lenght-1]
@@ -111,13 +114,15 @@ def unlocking_char(x, list, lock, duplicated_char):
     return lock 
 
 def play_again():
-    question = "j"
+    question = ""
     while question != 'a' or 'q':
         question = input("press 'a' to play again or 'q' to quit: ")
         if question == 'a':
             main()
         elif question == 'q':
             quit()
+        else:
+            play_again()
 
 def attempts_counter(counter):
     counter += 1
@@ -128,10 +133,23 @@ def starting_time():
     return start
 def ending_time(start_time):
     
-    end = time.time() - start_time
+    end = round(time.time() - start_time)
     return end
 
+
+
+def finish(correct_guess, start_time, counter, lotery_word):
+    if correct_guess >= len(lotery_word):
+        print(lotery_word)
+        print("CONGRATULATION YOU WON")
+        print(f"You played {ending_time(start_time)} seconds and tried giving correct letter {counter} times!")
+    else:
+        print(f"You played {ending_time(start_time)} seconds and tried giving correct letter {counter} times!")
+        print(f"You lost! your word was {lotery_word}")
+
+
 def main():
+    correct_guess = 0
     counter = 0
     live = 0
     start_time = starting_time()
@@ -143,19 +161,27 @@ def main():
     print(locked_word)
     while live <= 6:
         char = input(" Please give me a char: ")
+        clear()
         counter = attempts_counter(counter)
-        print(counter)
         if checking_char(char, lotery_word) is not False:
+            correct_guess += 1
+            if correct_guess == len(lotery_word):
+                break
+            print(hangman[live])
+            print(correct_guess)
             print("Congratulation!!!")
             duplicated_char=checking_char(char, lotery_word)
+            current_word_state = unlocking_char(char, lotery_word, locked_word, duplicated_char)
             print(unlocking_char(char, lotery_word, locked_word, duplicated_char))
         else:
-                print("kasujemy zycie")
-                print(hangman[live])
-                live += 1
-    print(ending_time(start_time))
-    print(f"You played {ending_time(start_time)} seconds and tried giving correct letter {counter} times!")
-    print("You lost!")
+            print("-1 live!")
+            print(hangman[live])
+            try:
+                print(current_word_state)
+            except:
+                UnboundLocalError
+            live += 1
+    finish(correct_guess, start_time, counter, lotery_word)
 
 main()
 play_again()
